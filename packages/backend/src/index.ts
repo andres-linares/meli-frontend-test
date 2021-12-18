db48e10a4e8ce2eef2ef9ace00a929fd2da5cc20
andres-linares2211 <andres.linares@omni.pro>
2021-12-18 09:47:09 -0500

    feat: add morgan for loggin incoming requests

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,11 +1,15 @@
 import express from 'express';
+import morgan from 'morgan';
 import config from 'config';
 import itemsController from './controllers/items-controller';
 
+const EXPRESS_PORT = config.get('expressPort') as number | 8080;
+const NODE_ENV = process.env.NODE_ENV || 'dev';
+
 const app = express();
+app.use(morgan(NODE_ENV === 'dev' ? 'dev' : 'common'));
 app.use(express.json());
 
-const EXPRESS_PORT = config.get('expressPort') as number | 8080;
 
 app.use('/api/items', itemsController);
 
