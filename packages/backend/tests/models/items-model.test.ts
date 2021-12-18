00b991049b962c4204f652aee8929b1af4f14bb0
andres-linares2211 <andres.linares@omni.pro>
2021-12-18 17:40:39 -0500

    test: add tests for happy path in getItemDetail

--- packages/backend/tests/models/items-model.test.ts
+++ packages/backend/tests/models/items-model.test.ts
@@ -1,9 +1,13 @@
 import axios from "axios";
 import config from "config";
-import { searchItems } from "../../src/models/items-model";
+import { getItemDetail, searchItems } from "../../src/models/items-model";
+
 import SearchResponseJson from "../fixtures/search-response.json";
 import SearchResponseGibberishJson from "../fixtures/search-response-gibberish.json";
 import SearchResponseExpressJson from "../fixtures/search-response-express.json";
+import ItemDetailJson from "../fixtures/item-detail.json";
+import ItemDetailDescriptionJson from "../fixtures/item-detail-description.json";
+import ItemDetailExpressJson from "../fixtures/item-detail-express.json";
 
 jest.mock("axios");
 
@@ -34,13 +38,49 @@ describe("ItemsModel", () => {
 
       expect(response).toEqual(SearchResponseExpressJson);
     });
-    
-    it('does not return items nor categories when api response is also empty', async () => {
+
+    it("does not return items nor categories when api response is also empty", async () => {
       mockedAxios.get.mockResolvedValueOnce({ data: SearchResponseGibberishJson });
       const response = await searchItems("wjiiojwerojiweoiwer");
 
       expect(response.categories.length).toEqual(0);
       expect(response.items.length).toEqual(0);
     });
+  });
+
+  describe("getItemDetail", () => {
+    beforeAll(() => {
+      jest.resetAllMocks();
+
+      mockedAxios.get.mockImplementation(async (url) => {
+        if (url.includes("/description")) return { data: ItemDetailDescriptionJson };
+
+        return { data: ItemDetailJson };
+      });
+    });
+
+    it("calls axios twice with correct urls", async () => {
+      const id = "MLA911456292";
+      await getItemDetail(id);
+
+      const expectedUrl = `${config.get("api")}/items/${id}`;
+
+      expect(mockedAxios.get.mock.calls).toEqual([[expectedUrl], [expectedUrl + "/description"]]);
+      expect(mockedAxios.get).toHaveBeenCalledTimes(2);
+    });
+
+    it("returns correct structure", async () => {
+      const response = await getItemDetail("MLA911456292");
+
+      expect(response).toEqual(ItemDetailExpressJson);
+    });
+
+    // it('does not return items nor categories when api response is also empty', async () => {
+    //   mockedAxios.get.mockResolvedValueOnce({ data: SearchResponseGibberishJson });
+    //   const response = await searchItems("wjiiojwerojiweoiwer");
+
+    //   expect(response.categories.length).toEqual(0);
+    //   expect(response.items.length).toEqual(0);
+    // });
   });
 });
