dcb84bbc2ec98dd67b756e7eb2d20400165df558
andres-linares2211 <andres.linares@omni.pro>
2021-12-18 09:15:14 -0500

    refactor: use config for express port

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,11 +1,14 @@
 import express from 'express';
+import config from 'config';
 import itemsController from './controllers/items-controller';
 
 const app = express();
 app.use(express.json());
 
+const EXPRESS_PORT = config.get('expressPort') as number | 8080;
+
 app.use('/api/items', itemsController);
 
-app.listen(8080, () => {
-  console.log('[SERVER] Started Express app on port 8080')
+app.listen(EXPRESS_PORT, () => {
+  console.log(`[SERVER] Started Express app on port ${EXPRESS_PORT}`)
 });
