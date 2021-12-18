79314d187bcb973d8594057b59e842f7ed8ba0c9
andres-linares2211 <andres.linares@omni.pro>
2021-12-18 11:20:03 -0500

    feat: add all assets

--- packages/frontend/components/Header/Header.tsx
+++ packages/frontend/components/Header/Header.tsx
@@ -15,7 +15,7 @@ const Header = () => {
   return (
     <header className={styles.header}>
       <Link href="/">
-        <Image src="/Logo_ML.png" alt="MercadoLibre Logo" width={106} height={72} />
+        <Image src="/icons/Logo_ML.png" alt="MercadoLibre Logo" width={53} height={36} />
       </Link>
 
       <SearchForm onSubmit={onSubmitSearch}/>
