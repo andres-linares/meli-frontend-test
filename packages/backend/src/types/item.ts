037d95456443e804a068d25a6fe43b0094d530fe
andres-linares2211 <andres.linares@omni.pro>
2021-12-18 08:45:28 -0500

    feat: add typescript interfaces for api response

---
+++ packages/backend/src/types/item.ts
@@ -0,0 +1,19 @@
+interface Price {
+  currency: string;
+  amount: number;
+  decimals: number;
+}
+
+export interface Item {
+  id: string;
+  title: string;
+  price: Price;
+  picture: string;
+  condition: string;
+  free_shipping: boolean;
+}
+
+export interface ItemDetail {
+  sold_quantity: number;
+  description: string;
+}
---
+++ packages/backend/src/types/responses.ts
@@ -0,0 +1,13 @@
+import { Author } from "./author";
+import { Item, ItemDetail } from "./item";
+
+export interface SearchResponse {
+  author: Author;
+  categories: string[];
+  items: Item[];
+}
+
+export interface DetailResponse {
+  author: Author;
+  item: ItemDetail;
+}
