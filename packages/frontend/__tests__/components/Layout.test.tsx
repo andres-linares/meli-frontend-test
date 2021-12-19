d3723eab2b5aeadadfa3da935acc4bbd1a452d69
andres-linares2211 <andres.linares@omni.pro>
2021-12-19 16:13:27 -0500

    test: add tests for Layout

--- packages/frontend/__tests__/components/Layout.test.tsx
+++ packages/frontend/__tests__/components/Layout.test.tsx
@@ -1,9 +1,13 @@
 import { render, screen } from "@testing-library/react";
 import Layout from "../../components/Layout";
 
+jest.mock('../../components/Header', () => () => <header>Test Header</header>)
+
 describe('Layout', () => {
   it('contains a Header', () => {
+    render(<Layout></Layout>);
 
+    expect(screen.getByText('Test Header')).toBeTruthy();
   });
 
   it('renders the passed component', () => {
