3a27dba2bf596c5f7da2840305d1e221983f5056
andres-linares2211 <andres.linares@omni.pro>
2021-12-19 18:17:50 -0500

    test: add more tests to SearchForm

--- packages/frontend/__tests__/components/Header/SearchForm.test.tsx
+++ packages/frontend/__tests__/components/Header/SearchForm.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import SearchForm from '../../../components/Header/SearchForm';
 
 jest.mock('next/image', () => () => <img />);
@@ -7,7 +7,7 @@ describe('SearchForm', () => {
   it('will call onSubmit with thei input value when form is submitted', async () => {
     const onSubmit = jest.fn();
     const form = render(<SearchForm onSubmit={onSubmit} placeholder='Buscar' />);
-    const input = await form.findByLabelText('¿Qué estás buscando?');
+    const input = await form.findByPlaceholderText('Buscar');
 
     fireEvent.change(input, { target: { value: 'ropa' }});
     fireEvent.submit(input);
@@ -22,6 +22,20 @@ describe('SearchForm', () => {
     fireEvent.click(button);
 
     expect(onSubmit).not.toHaveBeenCalled();
+  });
+
+  it('will use the placeholder passed as a prop in the input field', async () => {
+    const form = render(<SearchForm onSubmit={jest.fn()} placeholder='Test PL' />);
+    const input = await form.findByPlaceholderText('Test PL');
+
+    expect(input).toBeTruthy();
+  });
+
+  it('will use the initialValue passed as a prop in the input field', async () => {
+    const form = render(<SearchForm onSubmit={jest.fn()} placeholder='Test' initialValue='carro'  />);
+    const input = await form.findByPlaceholderText('Test');
+
+    expect(input.getAttribute('value')).toEqual('carro');
   });
 });
 
